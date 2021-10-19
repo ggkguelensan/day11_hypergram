8e1ecc4b5a3c72390c7948d8c1fa058dd2bd2923
ggkguelensan <gkguelensan@gmail.com>
2021-10-19 18:16:22 +0300

    slider handler

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let brightness_slider   = document.getElementById("brightness");
 let contrast_slider     = document.getElementById("contrast");
 let transparent_slider  = document.getElementById("transparent");
 
-function Truncate(num){
+function truncate(num){
     if(num < 0) num = 0;
     if(num > 255) num = 255;
     return num;
@@ -30,33 +30,26 @@ file_input.addEventListener("change", (event_change) => {
                 const image_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
                 const initial_pixels = [...image_data.data];
 
-                brightness_slider.addEventListener("change", () => {
-                    let Brightness = brightness_slider.value;
-                    console.log(Brightness);
+                const slider_handler = () => {
+                    let Contrast = parseFloat(contrast_slider.value);
+                    let Brightness = parseFloat(brightness_slider.value);
+                    let Factor = 259 * (255 + Contrast) / (255 * (259 - Contrast));
+                    console.log(Brightness, Contrast, Factor);
                     for(let i = 0; i < initial_pixels.length; i += 4) {
-                        image_data.data[i    ] = Truncate(initial_pixels[i    ] + Brightness);
-                        image_data.data[i + 1] = Truncate(initial_pixels[i + 1] + Brightness);
-                        image_data.data[i + 2] = Truncate(initial_pixels[i + 2] + Brightness);
+                        image_data.data[i    ] = truncate(Factor * (initial_pixels[i    ] - 128) + 128 + Brightness);
+                        image_data.data[i + 1] = truncate(Factor * (initial_pixels[i + 1] - 128) + 128 + Brightness);
+                        image_data.data[i + 2] = truncate(Factor * (initial_pixels[i + 2] - 128) + 128 + Brightness);
                     }
                     ctx.putImageData(image_data, 0, 0);
-                });
+                }
 
-                contrast_slider.addEventListener("change", () => {
-                    let Contrast = contrast_slider.value;
-                    console.log(Contrast);
-                    for(let i = 0; i < initial_pixels.length; i += 4) {
-                        let Factor = 259 * (255 + Contrast) / (255 * (259 - Contrast));
-                        image_data.data[i    ] = Truncate(Factor * (initial_pixels[i    ] - 128) + 128);
-                        image_data.data[i + 1] = Truncate(Factor * (initial_pixels[i + 1] - 128) + 128);
-                        image_data.data[i + 2] = Truncate(Factor * (initial_pixels[i + 2] - 128) + 128);
-                    }
-                    ctx.putImageData(image_data, 0, 0);
-                });
+                brightness_slider.addEventListener("change", slider_handler);
+                contrast_slider.addEventListener("change", slider_handler);
 
                 transparent_slider.addEventListener("change", () => {
                     let Transparent = transparent_slider.value;
                     console.log(Transparent);
-                    for(let i = 0; i < initial_pixels.length; i +=4 ) {
+                    for(let i = 0; i < initial_pixels.length; i += 4) {
                         image_data.data[i + 3] = initial_pixels[i + 3] * Transparent;
                     }
                     ctx.putImageData(image_data, 0, 0);
